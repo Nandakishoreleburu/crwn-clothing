735ecf0dc13082c87057832c462f50cce0f40f35
Nandakishoreleburu <nandakishoreleburu@gmail.com>
2021-11-17 10:01:17 +0530

    update our menu-items to navigate using usenavigate hoc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 // import logo from "./logo.svg";
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
+
+const Hatspage = () => (
+  <div>
+    <h1>hHATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      {" "}
-      <HomePage />{" "}
+      {/* <BrowserRouter> */}
+      {/* <Switch> */}
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/hats" element={<Hatspage />} />
+
+          {/* </Switch> */}
+          {/* </BrowserRouter> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ otherSectionProps }) => {
+  debugger;
+  const navigate = useNavigate();
+
+  return (
     <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${otherSectionProps.size} menu-item`}
+      onClick={() => navigate(`${otherSectionProps.linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${otherSectionProps.imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{otherSectionProps.title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
